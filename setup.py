b2d9743c4e46f7482ccfcef7d67629a27acf8d6d
Cameron Stitt <cameron@castitt.com>
2013-10-24 21:07:27 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='arnold',
-    version='0.1.0',
+    version='0.2.0',
     description='Simple migrations for python ORMs',
     long_description='',
     keywords='python, peewee, migrations',
