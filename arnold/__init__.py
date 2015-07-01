b489fbd6d6943952f296ac5baabfc145cdbc24d1
Cameron Stitt <cameron@cam.st>
2015-07-02 06:58:26 +1000

    Get tests passing.

--- arnold/__init__.py
+++ arnold/__init__.py
@@ -5,196 +5,177 @@ import argparse
 
 from termcolor import colored
 
-from arnold.exceptions import (
-    ArgumentException,
-    DBAttrNotFound,
-    DirectionNotFoundException,
-    InvalidConfiguration,
-    ModuleNotFoundException,
-    MigrationNotFoundException,
-)
+from arnold.exceptions import DirectionNotFoundException
 from arnold.models import Migration
 from importlib import import_module
 
 
-IGNORED_FILES = ["__init__"]
+class Terminator:
+    IGNORED_FILES = ["__init__"]
 
+    def __init__(self, args):
+        self.fake = getattr(args, 'fake', False)
+        self.count = getattr(args, 'count', 0)
+        self.folder = getattr(args, 'folder', None)
 
-def _setup_table(model):
-    if model.table_exists():
-        return
-    else:
-        model.create_table()
-    return True
+        self.prepare_config()
+        self.database = self.config.database
+        self.prepare_model()
 
+    def prepare_config(self):
+        self.config = import_module(self.folder)
 
-def _retreive_filenames(files):
-    filenames = list()
-    for f in files:
-        splits = f.rsplit(".", 1)
-        if len(splits) <= 1 or splits[1] != "py" or \
-           splits[0] in IGNORED_FILES:
-            continue
-        filenames.append(splits[0])
-    return sorted(filenames, key=lambda fname: int(fname.split("_")[0]))
+    def prepare_model(self):
+        self.model = Migration
+        self.model._meta.database = self.database
 
+        self._setup_table()
 
-def _perform_single_migration(direction, model, config, migration, args):
-    """Runs a single migration method (up or down)"""
-    fake = getattr(args, 'fake', False)
-
-    migration_exists = model.select().where(
-        model.migration == migration
-    ).limit(1).exists()
-
-    if migration_exists and direction == "up":
-        print("Migration {0} already exists, {1}".format(
-            colored(migration, "yellow"), colored("skipping", "cyan")
-        ))
-        return False
-    if not migration_exists and direction == "down":
-        print("Migration {0} does not exist, {1}".format(
-            colored(migration, "yellow"), colored("skipping", "cyan")
-        ))
-        return False
-
-    print("Migration {0} going {1}".format(
-        colored(migration, "yellow"), colored(direction, "magenta")
-    ))
-
-    if fake:
-        _update_migration_table(direction, model, migration)
-        print(
-            "Faking {0}".format(colored(migration, "yellow"))
-        )
-    else:
-        module_name = "{0}.{1}.{2}".format(
-            args.folder, 'migrations', migration
-        )
-        print("Importing {0}".format(
-            colored(module_name, "blue")
-        ))
-        try:
-            migration_module = import_module(module_name)
-        except ImportError:
-            raise ModuleNotFoundException(
-                "Migration {0} failed to import.", module_name
-            )
-
-        if hasattr(migration_module, direction):
-            getattr(migration_module, direction)()
-            _update_migration_table(direction, model, migration)
+    def _setup_table(self):
+        if self.model.table_exists():
+            return False
         else:
-            raise DirectionNotFoundException
-
-    print("Migration {0} went {1}".format(
-        colored(migration, "yellow"), colored(direction, "magenta")
-    ))
-    return True
-
-
-def _update_migration_table(direction, model, migration):
-    if direction == "up":
-        model.insert(migration=migration).execute()
-    else:
-        model.delete().where(
-            model.migration == migration
-        ).execute()
-
-
-def get_latest_migration(model):
-    return model.select().order_by(model.migration.desc()).first()
-
-
-def _perform_migrations(direction, model, config, args):
-    """
-    Find the migration if it is passed in and call the up or down method as
-    required. If no migration is passed, loop through list and find
-    the migrations that need to be run.
-    """
-    files = os.listdir('{0}/{1}'.format(args.folder, 'migrations'))
-    filenames = _retreive_filenames(files)
-
-    if direction == "down":
-        filenames.reverse()
-
-    if len(filenames) <= 0:
+            self.model.create_table()
         return True
 
-    start = 0
+    def _retreive_filenames(self, files):
+        filenames = list()
+        for f in files:
+            splits = f.rsplit(".", 1)
+            if len(splits) <= 1 or splits[1] != "py" or \
+               splits[0] in self.IGNORED_FILES:
+                continue
+            filenames.append(splits[0])
+        return sorted(filenames, key=lambda fname: int(fname.split("_")[0]))
 
-    latest_migration = get_latest_migration(model)
+    def _perform_single_migration(self, migration):
+        """Runs a single migration method (up or down)"""
+        migration_exists = self.model.select().where(
+            self.model.migration == migration
+        ).limit(1).exists()
 
-    if latest_migration:
-        migration_index = filenames.index(latest_migration.migration)
+        if migration_exists and self.direction == "up":
+            print("Migration {0} already exists, {1}".format(
+                colored(migration, "yellow"), colored("skipping", "cyan")
+            ))
+            return False
+        if not migration_exists and self.direction == "down":
+            print("Migration {0} does not exist, {1}".format(
+                colored(migration, "yellow"), colored("skipping", "cyan")
+            ))
+            return False
 
-        if migration_index == len(filenames) - 1 and direction == 'up':
-            print("Nothing to go {0}.".format(colored(direction, "magenta")))
+        print("Migration {0} going {1}".format(
+            colored(migration, "yellow"), colored(self.direction, "magenta")
+        ))
+
+        if self.fake:
+            self._update_migration_table(migration)
+            print(
+                "Faking {0}".format(colored(migration, "yellow"))
+            )
+        else:
+            module_name = "{0}.{1}.{2}".format(
+                self.folder, 'migrations', migration
+            )
+            print("Importing {0}".format(
+                colored(module_name, "blue")
+            ))
+            migration_module = import_module(module_name)
+
+            if hasattr(migration_module, self.direction):
+                getattr(migration_module, self.direction)()
+                self._update_migration_table(migration)
+            else:
+                raise DirectionNotFoundException
+
+        print("Migration {0} went {1}".format(
+            colored(migration, "yellow"), colored(self.direction, "magenta")
+        ))
+        return True
+
+    def _update_migration_table(self, migration):
+        if self.direction == "up":
+            self.model.insert(migration=migration).execute()
+        else:
+            self.model.delete().where(
+                self.model.migration == migration
+            ).execute()
+
+    def get_latest_migration(self):
+        return self.model.select().order_by(
+            self.model.migration.desc()
+        ).first()
+
+    def perform_migrations(self, direction):
+        """
+        Find the migration if it is passed in and call the up or down method as
+        required. If no migration is passed, loop through list and find
+        the migrations that need to be run.
+        """
+        self.direction = direction
+
+        files = os.listdir('{0}/{1}'.format(self.folder, 'migrations'))
+        filenames = self._retreive_filenames(files)
+
+        if self.direction == "down":
+            filenames.reverse()
+
+        if len(filenames) <= 0:
             return True
 
-        if direction == 'up':
-            start = migration_index + 1
-        else:
-            start = migration_index
-    if not latest_migration and direction == 'down':
-        print("Nothing to go {0}.".format(colored(direction, "magenta")))
-        return True
+        start = 0
 
-    end = start + args.count
-    migrations_to_complete = filenames[start:end]
+        latest_migration = self.get_latest_migration()
 
-    if args.count > len(migrations_to_complete):
-        print(
-            "Count {0} greater than available migrations. Going {1} {2} times."
-            .format(
-                colored(args.count, "green"),
-                colored(direction, "magenta"),
-                colored(len(migrations_to_complete), "red"),
+        if latest_migration:
+            migration_index = filenames.index(latest_migration.migration)
+
+            if migration_index == len(filenames) - 1 and \
+                self.direction == 'up':
+                print("Nothing to go {0}.".format(
+                    colored(self.direction, "magenta"))
+                )
+                return False
+
+            if self.direction == 'up':
+                start = migration_index + 1
+            else:
+                start = migration_index
+        if not latest_migration and self.direction == 'down':
+            print("Nothing to go {0}.".format(
+                colored(self.direction, "magenta"))
             )
-        )
+            return False
 
-    for migration in migrations_to_complete:
-        _perform_single_migration(
-            direction, model, config, migration, args
-        )
-    return True
+        end = start + self.count
+        migrations_to_complete = filenames[start:end]
 
+        if self.count > len(migrations_to_complete):
+            print(
+                "Count {0} greater than available migrations. Going {1} {2} times."
+                .format(
+                    colored(self.count, "green"),
+                    colored(self.direction, "magenta"),
+                    colored(len(migrations_to_complete), "red"),
+                )
+            )
 
-def prepare_model(database):
-    model = Migration
-    model._meta.database = database
-
-    _setup_table(model)
-
-    return model
-
-
-def prepare_config(module):
-    try:
-        return import_module(module)
-    except ImportError:
-        raise ModuleNotFoundException("Config module could not be found.")
-
-
-def prepare_cmd(args):
-    config = prepare_config(args.folder)
-    model = prepare_model(config.database)
-    return config, model
+        for migration in migrations_to_complete:
+            self._perform_single_migration(migration)
+        return True
 
 
 def up(args):
-    config, model = prepare_cmd(args)
-    _perform_migrations('up', model, config, args)
+    Terminator(args).perform_migrations('up')
 
 
 def down(args):
-    config, model = prepare_cmd(args)
-    _perform_migrations('down', model, config, args)
+    Terminator(args).perform_migrations('down')
 
 
 def status(args):
-    config, model = prepare_cmd(args)
-    latest_migration = get_latest_migration(model)
+    latest_migration = Terminator(args).get_latest_migration()
     if latest_migration:
         print("Migration {0} run at {1}.".format(
             colored(latest_migration.migration, "blue"),
@@ -204,7 +185,7 @@ def status(args):
         print("No migrations currently run.")
 
 
-def main():
+def parse_args(args):
     sys.path.insert(0, os.getcwd())
     parser = argparse.ArgumentParser(description='Migrations. Down. Up.')
     parser.add_argument(
@@ -233,5 +214,8 @@ def main():
         'count', type=int, help='How many migrations to go down.'
     )
 
-    args = parser.parse_args()
+    return parser.parse_args(args)
+
+def main():
+    args = parse_args(sys.argv)
     args.func(args)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,30 +1,15 @@
+import os
+import sys
 import unittest
 
 from peewee import SqliteDatabase, Model
 
-from arnold import (
-    _perform_migrations, _perform_single_migration, _setup_table, main
-)
-from arnold.exceptions import (
-    DBAttrNotFound,
-    FieldNotFoundException,
-    FieldsRequiredException,
-    InvalidConfiguration,
-    ModuleNotFoundException
-)
+from arnold import Terminator, parse_args
 from arnold.models import Migration
-from arnold.utils import create_table
 
+#sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 db = SqliteDatabase('test.db')
-
-directory = "tests/migrations"
-migration_module = "tests.migrations"
-
-kwargs = {
-    'directory': directory,
-    'migration_module': migration_module
-}
 
 
 class BasicModel(Model):
@@ -35,6 +20,8 @@ class TestMigrationFunctions(unittest.TestCase):
     def setUp(self):
         self.model = Migration
         self.model._meta.database = db
+        if self.model.table_exists():
+            self.model.drop_table()
         self.model.create_table()
 
         self.good_migration = "001_initial"
@@ -49,22 +36,22 @@ class TestMigrationFunctions(unittest.TestCase):
         # Drop the table if it exists, as we are creating it later
         if self.model.table_exists():
             self.model.drop_table()
-
-        self.assertTrue(_setup_table(self.model))
-        self.assertTrue(_setup_table(self.model) is None)
+        args = parse_args(['status'])
+        Terminator(args)
+        self.assertEqual(self.model.table_exists(), True)
 
     def do_good_migration_up(self):
         """A utility to perform a successfull upwards migration"""
-        self.assertTrue(_perform_single_migration(
-            "up", self.model, migration=self.good_migration, **kwargs
-        ))
+        args = parse_args(['up', '1'])
+        termi = Terminator(args)
+        termi.perform_migrations('up')
         self.assertTrue("basicmodel" in db.get_tables())
 
     def do_good_migration_down(self):
         """A utility to perform a successfull downwards migration"""
-        self.assertTrue(_perform_single_migration(
-            "down", self.model, migration=self.good_migration, **kwargs
-        ))
+        args = parse_args(['down', '1'])
+        termi = Terminator(args)
+        termi.perform_migrations('down')
         self.assertFalse("basicmodel" in db.get_tables())
 
     def test_perform_single_migration(self):
@@ -75,16 +62,18 @@ class TestMigrationFunctions(unittest.TestCase):
         """Run migration twice, second time should return False"""
         self.do_good_migration_up()
 
-        self.assertFalse(_perform_single_migration(
-            "up", self.model, migration=self.good_migration, **kwargs
-        ))
+        args = parse_args(['up', '1'])
+        termi = Terminator(args)
+        self.assertFalse(termi.perform_migrations('up'))
 
     def test_perform_single_migration_not_found(self):
         """Ensure that a bad migration argument raises an error"""
+        args = parse_args(['up', '1'])
+        termi = Terminator(args)
+
         with self.assertRaises(ImportError):
-            _perform_single_migration(
-                "up", self.model, migration=self.bad_migration, **kwargs
-            )
+            termi.direction = 'up'
+            termi._perform_single_migration('up')
 
     def test_perform_single_migration_down(self):
         """A simple test of _perform_single_migration down"""
@@ -94,9 +83,10 @@ class TestMigrationFunctions(unittest.TestCase):
 
     def test_perform_single_migration_down_does_not_exist(self):
         """Ensure False response when migration isn't there"""
-        self.assertFalse(_perform_single_migration(
-            "down", self.model, migration=self.good_migration, **kwargs
-        ))
+        args = parse_args(['down', '1'])
+        termi = Terminator(args)
+
+        self.assertFalse(termi.perform_migrations('down'))
 
     def test_perform_single_migration_adds_deletes_row(self):
         """Make sure that the migration rows are added/deleted"""
@@ -112,73 +102,12 @@ class TestMigrationFunctions(unittest.TestCase):
             self.model.migration == self.good_migration
         ).limit(1).exists())
 
-    def test_perform_migrations(self):
-        """A simple test of _perform_migrations"""
-        self.assertEqual(_perform_migrations(
-            "up", self.model, **kwargs
-        ), True)
-
-    def test_perform_migrations_with_migration_argument(self):
-        """A simple test of _perform_migrations with a migration argument"""
-        self.assertTrue(_perform_migrations(
-            "up", self.model, migration=self.good_migration, **kwargs
-        ))
-
-    def test_main_single_migration(self):
-        """Call arnold's main method and perform a single migration"""
-        self.assertTrue(main(database=db, **kwargs))
-
-    def test_main_ignore_migration(self):
-        """If we pass files to ignore, make sure they aren't run"""
-        self.assertTrue(main(database=db, ignored=["001_initial"], **kwargs))
-        self.assertFalse(self.model.select().where(
-            self.model.migration == self.good_migration
-        ).limit(1).exists())
-
-    def test_main_no_database_raise_error(self):
-        """If we don't pass a database in, raise an error"""
-        with self.assertRaises(DBAttrNotFound):
-            main(**kwargs)
-
-    def test_no_directory_or_module_raises_error(self):
-        """If we don't pass directory or module, raise an error"""
-        # Try without directory first
-        with self.assertRaises(InvalidConfiguration):
-            main(database=db, migration_module=migration_module)
-
-        # Try without module
-        with self.assertRaises(InvalidConfiguration):
-            main(database=db, directory=directory)
-
     def test_with_fake_argument_returns_true_no_table(self):
         """If we pass fake, return true, but don't create the model table"""
-        kwargs.update({
-            'fake': True
-        })
-        self.assertTrue(main(database=db, **kwargs))
+        args = parse_args(['up', '1', '--fake', 'true'])
+        termi = Terminator(args)
+        self.assertTrue(termi.perform_migrations('up'))
         self.assertFalse("basicmodel" in db.get_tables())
-
-
-class TestUtilMethods(unittest.TestCase):
-    def setUp(self):
-        self.model = Migration
-        self.model._meta.database = db
-
-    def tearDown(self):
-        if self.model.table_exists():
-            self.model.drop_table()
-
-    def test_create_table(self):
-        create_table(self.model, ["id", "migration"])
-        self.assertTrue(self.model.table_exists())
-
-    def test_create_table_fails_on_unkown_field(self):
-        with self.assertRaises(FieldNotFoundException):
-            create_table(self.model, ["id", "migration", "bad_field"])
-
-    def test_create_table_with_empty_fields_raises_exception(self):
-        with self.assertRaises(FieldsRequiredException):
-            create_table(self.model, [])
 
 if __name__ == '__main__':
     unittest.main()
