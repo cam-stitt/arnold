8ba3313d3b28ae356cd473db60738b9687a3566e
Cameron Stitt <cameron.stitt@simplehq.com.au>
2016-01-12 10:20:28 +1000

    Bump to 0.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='arnold',
-    version='0.4.0',
+    version='0.4.1',
     description='Simple migrations for python ORMs',
     long_description='',
     keywords='python, peewee, migrations',
